2708c248107d4329f0b70be9b68915db60e327d5
gym1702 <89650800+gym1702@users.noreply.github.com>
2023-03-24 22:42:34 -0600

    bueno hasta boton stock clase:149

--- applications/productos/models.py
+++ applications/productos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 from applications.categorias.models import Categoria
 
 
@@ -17,6 +19,14 @@ class Producto(models.Model):
 
     class Meta:
         verbose_name_plural = 'Productos'
+
+
+    #utilizado para enviar ruta de detalle de producto
+    def get_url(self):
+        return reverse('productos_app:producto_detalle', args=[self.categoria.slug, self.slug])
+        #Genera una url: http://localhost:8000/tienda/categoria_slug/slug
+
+
 
     def __str__(self):
         return self.nombre
